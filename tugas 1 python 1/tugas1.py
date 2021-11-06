8566810ab9694bd6f0856057a0d568d24daa628a
Rizky1408 <agungdwisatria23@gmail.com>
2021-11-06 23:26:11 +0800

    uploud

---
+++ tugas 1 python 1/tugas1.py
@@ -0,0 +1,16 @@
+print("1. Program Pertama Python\n2. Variabel dan Tipe Data\n3. Menerima Input User\n4. Type Conversion")
+pilihan = int(input("\nMasukan Pilihan : "))
+
+if pilihan == 1 :
+    print("Hello World")
+    print("\nUntuk mengeluarkan Hello Word atau mencetak Hello world menggunakan fungsi print()")
+elif pilihan == 2 :
+    print('a = 2\nb = 10\nc ="Rizky"')
+    print("\n# Dalam pemograman python tidak terdapat deklarasi, yang ada hanyalahh definisi")
+    print("# dikarenakan python akan otomatis mendeteksi tipe data dari suatu variabel jika")
+    print("# kita memberikan nilainya.Lalu apa itu variabel dan tipe data?")
+    print("# Variabel dalam pemograman merupakan sebuah wadah yang di gunakan untuk menyimpan suatu value/nilai")
+    print("# sedangkan tipe data merupakan jenis nilai atau value atau data yang ada di dalam variabel")
+    print("# dikarenakan program tidak tau apa itu angka,string,pecahan,dan bulat, oleh karena itu")
+    print("# kita harus memeberi tahu program dengan cara seperti itu, tetapi hebatnya python")
+    print("# dia sudah otomatis mendefinisikan tipe data dari variabel yang sudah kita beri valuenya")
